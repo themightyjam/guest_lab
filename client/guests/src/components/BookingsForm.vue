bc32bb2505a9e5050449c9513169a4a3ba153880
Shaun Jerdan <s.jerdan93@gmail.com>
2020-04-22 10:49:23 +0100

    bookingsform.vue complete

--- client/guests/src/components/BookingsForm.vue
+++ client/guests/src/components/BookingsForm.vue
@@ -19,9 +19,56 @@
 </template>
 
 <script>
-export default {
+import { eventBus } from '../main';
+import BookingService from '..services/BookingService';
+
+expot default {
+  name: "bookings-form",
+  data(){
+  return {
+    name: "",
+    room: null,
+    date: null
+  }
+},
+methods: {
+  handleSubmit(event){
+    event.preventDefault();
+    const payload = {
+      name: this.name,
+      room: this.room,
+      date: this.date
+    };
+
+    BookingService.postBooking(payload)
+    .then(booking => {
+      eventBus.$emit('booking-added', booking);
+    });
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+h2 {
+	margin: 10px 0;
+	padding: 0;
+}
+
+form {
+	width: 75%;
+	margin: 0 auto;
+	background: rgba(255, 255, 255, 0.7);
+	padding: 20px;
+	margin-bottom: 40px;
+}
+
+label {
+	min-width: 100px;
+	display: inline-block;
+}
+
+.formWrap {
+	margin-bottom: 10px;
+}
 </style>
