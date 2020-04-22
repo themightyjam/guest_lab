10af862a7eea33b2a9258ab671f36e93391bbf88
Shaun Jerdan <s.jerdan93@gmail.com>
2020-04-22 10:15:37 +0100

    app.vue setup

--- client/guests/src/App.vue
+++ client/guests/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <guest-form />
+    <guest-gird />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GuestForm from './components/GuestForm.vue';
+import GuestGrid from './components/GuestGrid.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'guest-form': GuestForm,
+    'guest-grid': GuestGrid
   }
 }
 </script>
