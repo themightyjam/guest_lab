13cd92a26e495495e1f76c983b804d046eba8be2
Shaun Jerdan <s.jerdan93@gmail.com>
2020-04-22 11:01:57 +0100

    bookingsgrid setup

---
+++ client/guests/src/components/BookingsGrid.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div id="bookingsGrid">
+    <booking v-for="booking, index in bookings" :key="index"
+    :booking="booking" />
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main';
+import BookingService from '../services/BookingService'
+import Booking from './Booking';
+export default {
+  name: "bookings-grid",
+  data () {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+    'booking': Booking
+  },
+  mounted(){
+    this.fetchData();
+    eventBus.$on('booking-added', booking => this.bookings.push(booking));
+
+    eventBus.$on('booking-deleted', id => {
+      const index = this.booking.findIndex(booking => booking._id === id);
+      this.bookings.splice(index, 1);
+    })
+  },
+  mehtods: {
+    fetchData(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#sightingsGrid {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+}
+</style>
